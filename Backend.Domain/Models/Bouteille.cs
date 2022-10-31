d143f3022f7c0d41cb49a13e366efe9845196a77
Clement Tomagra <c.tomagra@afelio.be>
2022-10-31 11:39:29 +0100

    mise en place des models

--- Backend.Domain/Models/Bouteille.cs
+++ Backend.Domain/Models/Bouteille.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Backend.Domain.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Backend.Domain.Models
 {
     public class Bouteille : BaseModel
     {
+        public string Nom { get; set; } = String.Empty;
+        public Couleur Couleur { get; set; } = Couleur.Blanc;
+        public Pays Pays { get; set; } = Pays.Italie;
+        public DateTime? DateAchat { get; set; } = null;
+        public DateTime AnneeMaxPourLaBoire { get; set; } = DateTime.Now;
+
+        public Cave Cave { get; set; } = null!;
     }
 }
---
+++ Backend.Domain/Models/Enums/Pays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Models.Enums
+{
+    public enum Pays
+    {
+        Italie = 0,
+        France = 1,
+        Espagne = 2,
+        USA = 3,
+        Argentine = 4,
+        Australie = 5,
+        Chili = 6,
+        AfriqueduSud = 7,
+        Chine = 8,
+        Allemagne = 9,
+        Portugal = 10,
+        Russie = 11,
+        Roumanie = 12,
+        Hongrie = 13,
+        NouvelleZelande = 14,
+        Bresil = 15,
+        Autriche = 16,
+        Grece = 17,
+        Georgie = 18,
+        Suisse = 19,
+    }
+}
